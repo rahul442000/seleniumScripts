dda36f8d9c51707d18e96aeff0d9080d9cc8aef8
rahul44200 <rahulsoni442000@gmail.com>
2023-01-29 14:34:39 +0530

    testng

--- com.maven.rahul/src/main/java/com/maven/com/maven/rahul/ScriptOne.java
+++ com.maven.rahul/src/main/java/com/maven/com/maven/rahul/ScriptOne.java
@@ -1,37 +1,36 @@
 package com.maven.com.maven.rahul;
-
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
-
 
 public class ScriptOne {
-	
-	WebDriver driver;
+	WebDriver driver ;
+	@Test
+	public void f() {
+	  
+	    String baseUrl = "https://www.toolsqa.com/";
+	      
+	         
+	        System.out.println("Launching Google Chrome browser");
+	        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+			driver = new ChromeDriver();
+	        driver.get(baseUrl);
+	        String testTitle = "Tools QA";
+	        String originalTitle = driver.getTitle();
+	        Assert.assertEquals(originalTitle, testTitle);
+  }
 	
 	@BeforeMethod
-	
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("www.google.com");
-		driver.manage().window().maximize();
-	}
-	
-	@Test
-	public void verifyTitle() {
-		String str = driver.getTitle();
-		String tle = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
-		Assert.assertEquals(str, tle);
-		
+	public void beforeMethod() {
+		System.out.println("Starting Test On Chrome Browser");
 	}
 	
 	@AfterMethod
-	public void endTest() {
-		driver.quit();
+	public void afterMethod() {
+		 driver.close();
+		System.out.println("Finished Test On Chrome Browser");
 	}
-
 }
